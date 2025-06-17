bcd3cebca62b3cd4cf44d661b468bf317c0e36fe
wcoder547 <mw5667155@gmail.com>
2025-06-17 11:55:37 +0500

    Refactor path module examples and enhance instructions for ES Modules

--- path/index.js
+++ path/index.js
@@ -4,12 +4,15 @@ import path from "path";
 const app = express();
 const port = 3000;
 
+//Used in ES Modules
+// console.log("DirName:-" + import.meta.dirname);
+// console.log("FileName:-" + import.meta.url);
+
 app.get("/", (req, res) => {
   const filepath = "/user/john/docs/report.pdf";
   console.log("Basename" + path.basename(filepath));
   console.log("Dirname" + path.dirname(filepath));
   console.log("Extname" + path.extname(filepath));
-
   const parsed = path.parse(filepath);
   console.log(parsed);
   //   {
@@ -23,11 +26,10 @@ app.get("/", (req, res) => {
 
 //path.resolve
 const absolutePath = path.resolve("public", "file.txt");
-console.log(absolutePath);
-
+console.log("Absolute Path: " + absolutePath);
 //joined path
 const joinedPath = path.join("/user", "john", "docs", "report.pdf");
-console.log(joinedPath);
+console.log("Joined Path: " + joinedPath);
 
 app.listen(port, () => {
   console.log(`App is listening on http://localhost:${port}`);
